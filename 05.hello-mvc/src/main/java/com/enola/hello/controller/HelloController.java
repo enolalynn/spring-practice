1b8b3b6a3dec2fc606fd80dbaf32997530ca7dbf
KKhineLynn <KKhineLynn@2023S2Dell-016>
2024-04-18 16:19:09 +0630

    second

--- 05.hello-mvc/src/main/java/com/enola/hello/controller/HelloController.java
+++ 05.hello-mvc/src/main/java/com/enola/hello/controller/HelloController.java
@@ -1,16 +1,23 @@
-package com.enola.hello;
+package com.enola.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.enola.hello.model.HelloService;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
+	
+	@Autowired
+	private HelloService service;
+	
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("message","Hello from Spring Controller");
+		model.addAttribute("message",service.getMessage());
 		return "index";
 	}
 }
